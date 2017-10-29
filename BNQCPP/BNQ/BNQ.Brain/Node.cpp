a592a34e224e3c0da4decdffb6ff96625fdf18e4
Dimitar Dimitrov <dandreevd@gmail.com>
2017-10-29 19:11:20 +0200

    Added Node.

---
+++ BNQCPP/BNQ/BNQ.Brain/Node.cpp
@@ -0,0 +1,101 @@
+#include "Node.h"
+
+int Node::TotalVisits;
+
+Node::Node(std::shared_ptr<State> state) :
+	state(state)
+{
+}
+
+bool Node::IsLeaf() const
+{
+	return children.size() == 0;
+}
+
+bool Node::Visited() const
+{
+	return visits > 0;
+}
+
+double Node::UCB() const
+{
+	double exploitationTerm = value / visits;
+	double explorationTerm = std::sqrt(std::log(TotalVisits) / visits);
+	double UCB = exploitationTerm + ExplorationConstant * explorationTerm;
+
+	return UCB;
+}
+
+double Node::Value() const
+{
+	return value;
+}
+
+void Node::UpdateVisits()
+{
+	++visits;
+}
+
+void Node::UpdateValue(double value)
+{
+	this->value += value;
+}
+
+Node Node::Expand()
+{
+	if (!IsLeaf() || children.size() > 0)
+	{
+		throw std::logic_error("Cannot expand an already expanded node.");
+	}
+
+	switch (state->Type())
+	{
+	case StateType::Opponent:
+	{
+	}
+	case StateType::SingleAction:
+	{
+		ChoiceState* choiceState = (ChoiceState*)NextState();
+
+		for (Action action : choiceState->Actions())
+		{
+			auto actionState = std::make_shared<ActionState>(state, action);
+			Node child{ actionState };
+
+			children.push_back(child);
+		}
+
+		return children[0];
+	}
+	}
+}
+
+void Node::Simulate()
+{
+
+}
+
+std::vector<Node>& Node::Children()
+{
+	return children;
+}
+
+Node& Node::operator=(const Node& rhs)
+{
+	state = rhs.state;
+	visits = rhs.visits;
+	value = rhs.value;
+	children = rhs.children;
+
+	return *this;
+}
+
+State* Node::CurrentState() const
+{
+	return dynamic_cast<State*>(state.get());
+}
+
+State* Node::NextState() const
+{
+	return CurrentState()->NextState().get();
+}
---
+++ BNQCPP/BNQ/BNQ.Brain/Node.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <math.h>
+#include <memory>
+#include <vector>
+
+#include "ActionState.h"
+#include "ChoiceState.h"
+#include "OpponentState.h"
+#include "State.h"
+
+class Node
+{
+public:
+	Node() = default;
+	Node(std::shared_ptr<State> state);
+	bool IsLeaf() const;
+	bool Visited() const;
+	double UCB() const;
+	double Value() const;
+	std::vector<Node>& Children();
+	void UpdateVisits();
+	void UpdateValue(double value);
+	Node Expand();
+	void Simulate();
+public:
+	Node& operator=(const Node& rhs);
+private:
+	static constexpr double ExplorationConstant = 2.0;
+	static int TotalVisits;
+private:
+	State* CurrentState() const;
+	State* NextState() const;
+private:
+	std::shared_ptr<State> state;
+	int visits = 0;
+	double value = 0.0;
+	std::vector<Node> children;
+};
