7cc1c1a85b634ce9f200878986b2192c0d410c97
Dimitar Dimitrov <dandreevd@gmail.com>
2017-11-05 09:35:27 +0200

    Replaced the return rvalue with an lvalue for clarity.

--- BNQCPP/BNQ/BNQ.Brain/HeroStrategy.cpp
+++ BNQCPP/BNQ/BNQ.Brain/HeroStrategy.cpp
@@ -14,9 +14,9 @@ double HeroStrategy::GetShowdownValue() const
 	auto board = lastState->GetBoard().get();
 	auto boardAsHand = board->GetBoardAsHand();
 	auto holding = lastState->ToAct().GetHolding();
+	omp::Hand card1 = Constants::PowerTwoIndices[holding.Card1];
+	omp::Hand card2 = Constants::PowerTwoIndices[holding.Card2];
+	double eval = evaluator.evaluate(boardAsHand + card1 + card2);
 
-	return evaluator.evaluate(
-		boardAsHand +
-		omp::Hand(Constants::PowerTwoIndices[holding.Card1]) +
-		omp::Hand(Constants::PowerTwoIndices[holding.Card2]));
+	return eval;
 }
